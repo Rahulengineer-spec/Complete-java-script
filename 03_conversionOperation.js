e3562238503f0ff8e5c853dcbbce79eef8bc71bc
Rahulengineer-spec <19BCS1677@cuchd.in>
2024-01-20 10:31:37 +0530

    dataTypes Conversion

---
+++ 03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score="98"
+//conversion of dataTypes
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+//adding two string 
+str1="rahul"
+str2=" kumar"
+
+console.log(str1+str2)
+
