47cfedc43cdcf1ab7fde752c5ecf444511ac26cd
Rahulengineer-spec <19BCS1677@cuchd.in>
2024-01-20 10:06:24 +0530

    dataTypes

---
+++ 01_variable.js
@@ -0,0 +1,4 @@
+const accountNumber=35017839890
+var emailId="rahul9271@outlook.com"
+let phoneNumber="9818490248"
+city="delhi"
---
+++ 02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treal all JS code as a newer version
+// alert(5+8) //we are using nodejs,not browser
+let name="rahul"
+let age=23
+let isLoggedIn=true 
+//number
+//bigint
+//string  " "
+//boolean true/false
+//null standlone value
+//undefined 
+//symbol -unique
+console.log(typeof name);
+console.log(typeof isLoggedIn);
